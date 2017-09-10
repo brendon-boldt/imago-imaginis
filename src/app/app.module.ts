10061ac8e527e0e706a9252dfc2231f696613686
Holayn <kwwong15@punahou.edu>
2017-09-10 19:54:02 -0400

    Added base profile component page and its html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
+import { ProfileComponent } from './profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class ProfileComponent {
+}
