b392253910c8b619b04d9a1fc9cbbd76937b7002
Holayn <kwwong15@punahou.edu>
2017-09-10 20:07:33 -0400

    Added profile component and its html. Added navigation between home and profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home.component';
+import { ProfileComponent } from './profile.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/app-home', pathMatch: 'full' },
   { path: 'app-home', component: HomeComponent },
+  { path: 'app-profile', component: ProfileComponent }
 ];
 
 @NgModule({
