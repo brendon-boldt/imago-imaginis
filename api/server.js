d3b50cbf2df65d45363c139ce18efabef35b94b8
Kai <kwwong1597@gmail.com>
2017-09-24 02:13:40 -0400

    Added api stuff. See the README in /api. #14

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mountRoutes = require('./app/routes');
+
+const app = express();
+// mountRoutes(app);
+
+const port = 8000;
+
+//This allows Express to process URL encoded forms on its own.
+//This way, we don't get undefined when receiving JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app);
+app.listen(port, () => {
+    console.log('We are live on ' + port);
+})
