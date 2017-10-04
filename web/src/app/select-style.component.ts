e22e6421098c311fe0fbc55187538cd993edcc40
Kai <kwwong1597@gmail.com>
2017-10-04 01:43:59 -0400

    Updated style selection to use Bootstrap and Angular

--- web/src/app/select-style.component.ts
+++ web/src/app/select-style.component.ts
@@ -11,4 +11,10 @@ import { RouterModule, Routes, Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class SelectStyleComponent {
+  freeUser: boolean = true;
+  selectedStyle: Object = {"style": "Select a style", "example":"assets/brush.png"};
+  styles: Array<Object> = [{"style":"Cubism", "example":"../assets/cubism.jpg"}, {"style":"Flowers", "example":"../assets/flowers.jpg"}, {"style":"Starry Night", "example":"../assets/starrynight.jpg"}, {"style":"Oil Painting", "example":"../assets/oil.jpg"}, {"style":"Impressionism", "example":"../assets/impress.jpg"}];
+  update = function(style){
+    this.selectedStyle = style;
+  }
 }
