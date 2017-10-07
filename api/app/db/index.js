02f3f9cb4f0380bdd1ab626c6d3fd62e76d1aa7b
brendon-boldt <brendon_boldt@yahoo.com>
2017-10-07 17:02:13 -0400

    Add unimplemented functions to stylizer DB connection

--- api/app/db/index.js
+++ api/app/db/index.js
@@ -1,43 +1,15 @@
-const { Pool, Client } = require('pg');
 
-//Client
-// const client = new Client({
-//     user: 'Holayn',
-//     host: 'localhost',
-//     database: 'testdb',
-//     password: 'potpal12',
-//     port: 5432,
-// });
-
-//Pool
-const pool = new Pool({
-    user: 'Holayn',
-    host: 'localhost',
-    database: 'testdb',
-    password: 'potpal12',
-    port: 5432,
-});
-
-// client.connect();
-// pool.connect(0);
-
-// const query = "SELECT * FROM books";
 
 module.exports = {
-    // query: (text, params, callback) => {
-    //     return pool.query(text, params, callback)
-    // }
-    query: function(text) {
-        return pool.query(text);
-    },
-    test: function(text) {
-        return console.log(text);
-    }
+
+	// Retrieve an image from the database
+	getImage: function(imageId) {
+		// DB HTTP calls
+	}
+
+	// Load an image into the database
+	sendImage: function(image) {
+		// DB HTTP calls
+	}
 }
 
-// client.query(query)
-//     .then(res => {
-//         console.log(res.rows[0]);
-//         client.end();
-//     })
-//     .catch(e => console.error(e.stack))
