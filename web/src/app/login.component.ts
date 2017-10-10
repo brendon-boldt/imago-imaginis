54112ac694ae235f5db7ca6793240e76cce098ef
Kai Wong <kai.wong@ibm.com>
2017-10-10 19:42:19 -0400

    Applied styling of home page to login page. Also, I realized we need a way for users to access their photos so created a placeholder library component

--- web/src/app/login.component.ts
+++ web/src/app/login.component.ts
@@ -11,4 +11,7 @@ import { RouterModule, Routes, Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class LoginComponent {
+  public keyboard: String = "../assets/keyboard.jpg";
+  public upload: String = "../assets/upload.jpg";
+  public style: String = "../assets/style.jpg";
 }
