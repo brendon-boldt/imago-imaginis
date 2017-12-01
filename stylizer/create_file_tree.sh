24186a6a525e9fe767188066b316e09bb23a962f
brendon-boldt <brendon_boldt@yahoo.com>
2017-12-01 18:58:06 -0500

    Add file tree creation script for stylizer and api

---
+++ stylizer/create_file_tree.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# $1 represents the root of the file tree
+
+mkdir "$1"
+mkdir "$1/images"
+mkdir "$1/images/uploads"
+mkdir "$1/images/filters"
+mkdir "$1/images/outputs"
+mkdir "$1/videos"
+mkdir "$1/videos/uploads"
+mkdir "$1/videos/outputs"
