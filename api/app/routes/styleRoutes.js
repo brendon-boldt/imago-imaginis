9bbce95bd803878cea95b8370e36c31ec062b083
Brendon Boldt <brendon.boldt@gmail.com>
2017-12-04 22:58:00 -0500

    Add comments to API style routes

--- api/app/routes/styleRoutes.js
+++ api/app/routes/styleRoutes.js
@@ -11,18 +11,17 @@ let options = {
 
 const config = require('../../config.js');
 
+/**
+ * If the user is not a paid user, a watermark should be applied
+ */
 let shouldWatermark = async function(resource_id, type){
   if (type !== 'image') {
     return false;
   }
-  // Return true if jwt is paid user, else return false
-  // Look up user id and see if they're a paid user
-  //let queryText = "SELECT * FROM ASP_USERS LEFT JOIN paid_users ON asp_users.user_id = paid_users.paid_id WHERE user_ID = $1;";
   let queryText = 'SELECT * FROM ASP_USERS LEFT JOIN paid_users ON asp_users.user_id = paid_users.paid_id LEFT JOIN user_photo ON (user_photo.user_id = paid_users.paid_id) WHERE photo_id = $1';
   let values = [resource_id];
   result = await db.param_query(queryText, values)
   console.log(result.rows[0])
-  //if(result.rows[0] !== null){
   console.log(result.rows);
   if(result.rows.length !== 0){
     console.log("VERIFY: Paid user, NO WATERMARK");
@@ -34,10 +33,16 @@ let shouldWatermark = async function(resource_id, type){
   }
 }
 
+/**
+ * Make sure that the authentication token is correct
+ */
 let verifyRequest = function(req) {
   return req.query.token === config.styleApiToken;
 }
 
+/**
+ * Template response to failed authentication 
+ */
 let verificationFailed = function(res) {
   res.status(401);
   res.statusMessage = "Unauthorized style request";
@@ -47,12 +52,15 @@ let verificationFailed = function(res) {
 
 module.exports = function(app) {
 
+  /**
+   * If any images/videos are stuck in the 'processing' state, set them to
+   * 'waiting' so that they can get styled
+   */
   app.get('/style/refresh/:type', async (req, getres) => {
     if (!verifyRequest(req)) {
       verificationFailed(getres);
       return;
     }
-
 
     let refreshVideos = "update user_video set status = 'waiting' where status = 'processing'";
     let refreshPhotos = "update user_photo set status = 'waiting' where status = 'processing'";
@@ -60,7 +68,7 @@ module.exports = function(app) {
       db.query(refreshPhotos);
     } else if (req.params.type === "videos") {
       db.query(refreshVideos);
-    } else {
+    } else if (req.params.type === "both") { 
       db.query(refreshPhotos);
       db.query(refreshVideos);
     }
@@ -69,13 +77,17 @@ module.exports = function(app) {
   });
 
 
+  /**
+   * Insert an image or video that has been styled
+   */
   app.post('/style/insert/:type*', async (req, getres) => {
     if (!verifyRequest(req)) {
       verificationFailed(getres);
       return;
     }
-    console.log("Upload of type: " + req.params.type);
 
+    console.log("Upload of type: " + req.params.type);
+    // The query will need to be different for an image or video
     let outputPath, bridgeTable, resultTable, resource_id_name;
     if (req.params.type === 'image') {
       outputPath = config.outputPath;
@@ -89,47 +101,47 @@ module.exports = function(app) {
       resource_id_name = 'video_id';
     }
 
-    console.log("QUERY: ", req.query);
     let filepath = `${outputPath}/output-${req.query.resource_id}.${req.query.fileType}`; 
     console.log(`Writing file: ${filepath}`);
     fs.writeFile(filepath, req.body, async (err) => {
-        if (err) {
-          throw err;    
+      if (err) {
+        throw err;    
+      }
+      //execFile('rm', [config.contentPath+`/upload-${uf_resource_id]);
+      getres.json({'status': 0});
+      try {
+        // If the user is a free user, watermark the styled image
+        if (await shouldWatermark(req.query.resource_id, req.params.type)) {
+          await watermark.embedWatermark(filepath, options);
         }
-        //execFile('rm', [config.contentPath+`/upload-${uf_resource_id]);
-        getres.json({'status': 0});
-        try {
-          if (await shouldWatermark(req.query.resource_id, req.params.type)) {
-            await watermark.embedWatermark(filepath, options);
-          }
-        }
-        catch(e){ console.log(e); }
-      });
+      }
+      catch(e){ console.log(e); }
+    });
 
     let resource_id = parseInt(req.query.resource_id);
-    console.log(resource_id);
     let user_id = parseInt(req.query.user_id);
     let resourceQuery = `UPDATE ${resultTable} SET (path, process_time)=($1,$2) WHERE ${resource_id_name} = $3`;
-    console.log(resourceQuery); 
     let resourceParams = [filepath, req.query.process_time, resource_id];
+    // Update the path and the processing time of the photo/video entry in the DB
     db.param_query(resourceQuery, resourceParams); 
 
     let bridgeQuery = `UPDATE ${bridgeTable} SET status='done' WHERE ${resource_id_name}=$1 AND user_id=$2`;
     let bridgeParams = [resource_id, user_id];
-    console.log(bridgeQuery); 
+    // Update the corresponding run information in the DB
     db.param_query(bridgeQuery, bridgeParams); 
     return 0;
   });
 
+  /**
+   * Retrieve an image from the DB
+   */
   app.post('/style/select/:type', (req, res) => {
     if (!verifyRequest(req)) {
       verificationFailed(getres);
       return;
     }
-    console.log("Received: ", req.body);
 
-  
-
+    // Determine how to name the file: image/video and content/style
     let filepath = 'UNSET';
     if (req.body.type === 'content') {
       let contentPath;
@@ -147,17 +159,20 @@ module.exports = function(app) {
 
       filepath = `${contentPath}/upload-${req.body.resource_id}.${req.body.fileType}`;
 
-    // TODO: This should be its own route, but I do not have time for that now
+      // If the style server is retrieving a content image/video, set the
+      // status of that run to 'processing' so that it will not be restyled
       let processingQuery = `UPDATE ${bridgeTable} SET status='processing' WHERE ${unfilteredId}=${req.body.resource_id}`;
-      console.log(processingQuery); 
       db.query(processingQuery); 
-    } else
+    } else {
       filepath = `${config.stylePath}/filter-${req.body.resource_id}.${req.body.fileType}`;
+    }
     console.log('Sending: ' + filepath);
     res.sendFile(filepath);
   });
 
-  // Not used?
+  /**
+   * Get the information for a given run in the DB
+   */
   app.post('/style/select/run', (req, getres) => {
     if (!verifyRequest(req)) {
       verificationFailed(getres);
@@ -179,7 +194,9 @@ module.exports = function(app) {
     return 0;
   });
 
-  // Multiple runs
+  /**
+   * Get all of the runs of one type (image/vide)
+   */
   app.post('/style/selectRuns/:type', (req, getres) => {
     if (!verifyRequest(req)) {
       verificationFailed(getres);
@@ -192,7 +209,6 @@ module.exports = function(app) {
       queryText = 'SELECT video_id, user_id, unfiltered_video_id, filters.filter_id, unfiltered_video.path AS uvpath, filters.path AS fpath FROM user_video NATURAL JOIN unfiltered_video JOIN filters ON (filters.filter_id = user_video.filter_id) WHERE status=\'waiting\'';
     }
 
-    //console.log("QUERYING: " + queryText);
     db.query(queryText)
       .then(res => {
         console.log(res.rows);
